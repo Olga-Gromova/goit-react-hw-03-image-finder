95330e997ee9c3693a3ec92d5498cc5a04f61559
Olga-Gromova <gromova.o.77@gmail.com>
2023-09-30 21:24:25 +0200

    updated_300923

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { AppStyle } from 'App.styled';
+import ImageGallery from 'components/ImageGallery/ImageGallery';
+import SearchBar from 'components/Searchbar/Searchbar';
+import { Component } from 'react';
+
+export default class App extends Component {
+  state = {
+    imageName: '',
+  };
+
+  getValuesForm = val => {
+    this.setState({ imageName: val });
+  };
+
+  render() {
+    return (
+      <AppStyle>
+        <SearchBar onSubmit={this.getValuesForm} />
+        <ImageGallery imageName={this.state.imageName} />
+      </AppStyle>
+    );
+  }
+}
